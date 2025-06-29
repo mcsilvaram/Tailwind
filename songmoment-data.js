d7e29f7ab4229c593aa1d56f0d687c67ec3dc1cc
mcsilvaram <seomcsilva@gmail.com>
2025-06-29 03:50:07 -0400

    Create songmoment-data.js

---
+++ songmoment-data.js
@@ -0,0 +1,117 @@
+// songmoment-data.js
+const canciones = [
+  {  //1
+    titulo: "la culpa (track 11)",
+    artistas: "Alleh, Yorghaki",
+    imagen: "./img_spotify/1/alleh.png",
+    alt: "Portada de la canción 'la culpa' de Alleh y Yorghaki"
+  },
+  {  //2
+    titulo: "Tendencia Nacional",
+    artistas: "Caleb Calloway, Alvaro Diaz, Rawayana",
+    imagen: "./img_spotify/1/alvaro.png",
+    alt: "Portada de la canción 'Tendencia Nacional' de Caleb Calloway, Alvaro Diaz y Rawayana"
+  },
+  {  //3
+    titulo: "Gohan y Goku Arcangel",
+    artistas: "Austin San",
+    imagen: "./img_spotify/1/austin.png",
+    alt: "Portada de la canción 'Gohan y Goku Arcangel' de Austin San"
+  },
+  {  //4
+    titulo: "Quédate",
+    artistas: "Beéle",
+    imagen: "./img_spotify/1/beele.png",
+    alt: "Portada de la canción 'Quedate' de Beéle"
+  },
+  {  //5
+    titulo: "NO ME ALCANZA",
+    artistas: "Duki",
+    imagen: "./img_spotify/1/duki.png",
+    alt: "Portada de la canción 'no me alcanza' de Duki"
+  },
+  {  //6
+    titulo: "Ponme En Tu Boca",
+    artistas: "Jerry Di, Danny Ocean",
+    imagen: "./img_spotify/1/j-hope.png",
+    alt: "Portada de la canción 'Ponme En Tu Boca' de Jerry Di, Danny Ocean"
+  },
+  {  //7
+    titulo: "Killin'It Girls",
+    artistas: "(feat. GloRilla) j-hope, GloRilla",
+    imagen: "./img_spotify/1/jerry.png",
+    alt: "Portada de la canción 'Killin'It Girls' de J(feat. GloRilla) j-hope, GloRilla"
+  },
+  {  //8
+    titulo: "Slow Dance",
+    artistas: "(feat. Sofia Carson) Jimin, Sofia Carson",
+    imagen: "./img_spotify/1/jimin.png",
+    alt: "Portada de la canción 'Slow Dance' de (feat. Sofia Carson) Jimin, Sofia Carson"
+  },
+  {  //9
+    titulo: "Verano Rosa",
+    artistas: "KAROL G, Feid",
+    imagen: "./img_spotify/1/karolg.png",
+    alt: "Portada de la canción 'Verano Rosa' de KAROL G, Feid"
+  },
+  {  //10
+    titulo: "Gabriela",
+    artistas: "KATSEYE",
+    imagen: "./img_spotify/1/katseye.png",
+    alt: "Portada de la canción 'Gabriela' de KATSEYE"
+  },
+  {  //11
+    titulo: "UNA NOCHE DE LOCURA",
+    artistas: "Blessd, Sebastian Ledher, Tayson Kryss,...",
+    imagen: "./img_spotify/1/ledher.png",
+    alt: "Portada de la canción 'UNA NOCHE DE LOCURA' de Blessd, Sebastian Ledher, Tayson Kryss,..."
+  },
+  {  //12
+    titulo: "Lover Girl",
+    artistas: "Laufey",
+    imagen: "./img_spotify/1/lover_girl.png",
+    alt: "Portada de la canción 'Lover Girl' de Laufey"
+  },
+  {  //13
+    titulo: "Love",
+    artistas: "Maybe MeloMance",
+    imagen: "./img_spotify/1/melomance.png",
+    alt: "Portada de la canción 'Love' de Maybe MeloMance"
+  },
+  {  //14
+    titulo: "Mente Loca",
+    artistas: "Neutro Shorty",
+    imagen: "./img_spotify/1/neutro.png",
+    alt: "Portada de la canción 'Mente Loca' de Neutro Shorty"
+  },
+  {  //15
+    titulo: "YO Y TU",
+    artistas: "OvyOn The Drums, Quevedo, Beéle",
+    imagen: "./img_spotify/1/quevedo.png",
+    alt: "Portada de la canción 'YO Y TU' de OvyOn The Drums, Quevedo, Beéle"
+  },
+  {  //16
+    titulo: "LOKENECESITAS SAIKO",
+    artistas: "Omar Courtz",
+    imagen: "./img_spotify/1/saiko.png",
+    alt: "Portada de la canción 'LOKENECESITAS SAIKO' de Omar Courtz"
+  },
+  {  //17
+    titulo: "we never dated",
+    artistas: "sombr",
+    imagen: "./img_spotify/1/sombr.png",
+    alt: "Portada de la canción 'we never dated' de sombr"
+  },
+  {  //18
+    titulo: "Universidad",
+    artistas: "TINI, Beéle",
+    imagen: "./img_spotify/1/tini.png",
+    alt: "Portada de la canción 'Universidad' de TINI, Beéle"
+  },
+  {  //19
+    titulo: "SOLEAO",
+    artistas: "Myke Towers, Quevedo",
+    imagen: "./img_spotify/1/transventis.png",
+    alt: "Portada de la canción 'SOLEAO' de Myke Towers, Quevedo"
+  }
+];
