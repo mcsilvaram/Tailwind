b109d590c4faec557747a653dd07962a8e999637
mcsilvaram <seomcsilva@gmail.com>
2025-06-30 12:40:47 -0400

    Create artistas.js

---
+++ folder_js/artistas.js
@@ -0,0 +1,63 @@
+// artistas.js
+const artistas = [
+  {  //1
+    nombre: "Kendrick Lamar",
+    descripcion: "Artista",
+    imagen: "./img_spotify/2/KLamar.png",
+    alt: "Kendrick Lamar Artista",
+  },
+  {  //2    
+    mombre: "Drake",
+    descripcion: "Artista",
+    imagen: "./img_spotify/2/Drake.png",
+    alt: "Drake Artista",
+  },
+  {  //3
+    nombre: "The Weeknd ",
+    descripcion: "Artista",
+    imagen: "./img_spotify/2/weeknd.png",
+    alt: "The Weeknd Artista",
+  },
+  {  //4    
+    nombre: " Morgan Wallen",
+    descripcion: "Artista",
+    imagen: "./img_spotify/2/MWallen.png",
+    alt: " Morgan Wallen Artista",
+  },
+  {  //5    
+    nombre: "Post Malone",
+    descripcion: "Artista",
+    imagen: "./img_spotify/2/Malone.png",
+    alt: "Post Malone Artista",
+  },
+  {  //6   
+    nombre: "Rihanna",
+    descripcion: "Artista",
+    imagen: "./img_spotify/2/Rihanna.png",
+    alt: "Rihanna Artista",
+  },
+  {  //7    
+    nombre: "Lady Gaga",
+    descripcion: "Artista",
+    imagen: "./img_spotify/2/lgaga.png",
+    alt: "Lady Gaga Artista",
+  },
+  {  //8    
+    nombre: "Billie Eilish",
+    descripcion: "Artista",
+    imagen: "./img_spotify/2/billieeilish.png",
+    alt: "Billie Eilish Artista",
+  },
+  {  //9    
+    nombre: "Taylor Swift",
+    descripcion: "Artista",
+    imagen: "./img_spotify/2/taylorswift.png",
+    alt: "Taylor Swift Artista",
+  },
+  {  //10
+    nombre: "Kanye West",
+    descripcion: "Artista",
+    imagen: "./img_spotify/2/kanyewest.png",
+    alt: "Kanye West Artista",
+  }
+];
