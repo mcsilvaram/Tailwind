820148e54e21b1a9bd8d57789961527b55f482f8
mcsilvaram <seomcsilva@gmail.com>
2025-07-02 17:35:31 -0400

    Create masescuchado.js

---
+++ folder_js/masescuchado.js
@@ -0,0 +1,53 @@
+// masescuchado.js
+const masescuchado = [
+  {  //1
+    nombre: "Los éxitos de hoy en un solo lugar! En...",
+    imagen: "./img_spotify/6/Losexitos.png",
+    alt: "Los éxitos",
+  },
+  {  //2    
+    nombre: "lo mejor nunca se subeLos éxitos de hoy en un solo lugar! En...",
+    imagen: "./img_spotify/6/lomejor.png",
+    alt: "lo mejor nunca",
+  },
+  {  //3
+    nombre: "Los éxitos de hoy en un solo lugar! En...",
+    imagen: "./img_spotify/6/Con Camilo.png",
+    alt: "Los éxitos",
+  },
+  {  //4    
+    nombre: "Los éxitos del pop latino, con Alleh &...",
+    imagen: "./img_spotify/6/poplatino.png",
+    alt: " Los éxitos del pop latino",
+  },
+  {  //5    
+    nombre: "Super mega hits! Foto: Sabrina Carpernter",
+    imagen: "./img_spotify/6/SabrinaCarpernter.png",
+    alt: "Super mega hits! Foto",
+  },
+  {  //6   
+    nombre: "Representado la cultura con orgullo...",
+    imagen: "./img_spotify/6/Representado.png",
+    alt: "Representado la cultura",
+  },
+  {  //7    
+    nombre: "Los temas que nunca para escuchar. E...",
+    imagen: "./img_spotify/6/Lostemasque.png",
+    alt: "Los temas que nunca",
+  },
+  {  //8    
+    nombre: "Las canciones que suenan en todas...",
+    imagen: "./img_spotify/6/Lascanciones.png",
+    alt: "Las canciones que suenan",
+  },
+  {  //9    
+    nombre: "Los éxitos de la salsa de ayer y hoy en un...",
+    imagen: "./img_spotify/6/salsadeayer.png",
+    alt: "Los éxitos de la salsa",
+  },
+  {  //10
+    nombre: "Estos son los hits que acompañan la fiesta!",
+    imagen: "./img_spotify/6/hitsqueacompañan.png",
+    alt: "Con Bad Bunny",
+  }
+];
