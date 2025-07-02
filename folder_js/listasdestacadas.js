5d227cbd307f89ff4b410fda80c6e6cf306ec107
mcsilvaram <seomcsilva@gmail.com>
2025-07-02 17:35:04 -0400

    Create listasdestacadas.js

---
+++ folder_js/listasdestacadas.js
@@ -0,0 +1,33 @@
+// listasdestacadas.js
+const listasdestacadas = [
+  {  //1
+    nombre: "Tu actualización semanal de las...",
+    imagen: "./img_spotify/5/Tuactualizacionsemanal1.png",
+    alt: "Tu actualización",
+  },
+  {  //2    
+    nombre: "Tu actualización semanal de las...",
+    imagen: "./img_spotify/5/Tuactualizacionsemanal2.png",
+    alt: "Tu actualización",
+  },
+  {  //3
+    nombre: "Tu actualización diaria de las canciones más...",
+    imagen: "./img_spotify/5/Tuactualizaciondiaria1.png",
+    alt: "Tu actualización diaria",
+  },
+  {  //4    
+    nombre: "Tu actualización diaria de las canciones más...",
+    imagen: "./img_spotify/5/Tuactualizaciondiaria2.png",
+    alt: "Tu actualización"
+},
+  {  //5    
+    nombre: "Tu actualización diaria de las canciones más...",
+    imagen: "./img_spotify/5/Tuactualizaciondiaria5.png",
+    alt: "Tu actualización diaria",
+  },
+  {  //6   
+    nombre: "Tu actualización diaria de las canciones más...",
+    imagen: "./img_spotify/5/Tuactualizaciondiaria6.png",
+    alt: "Tu actualización diaria",
+  }
+];
