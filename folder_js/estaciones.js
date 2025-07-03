daf78cf9729935b9c11f9b68acd08e0563eba13e
mcsilvaram <seomcsilva@gmail.com>
2025-07-03 11:52:13 -0400

    Update estaciones.js

--- folder_js/estaciones.js
+++ folder_js/estaciones.js
@@ -2,52 +2,52 @@
 const estaciones = [
   {  //1
     nombre: "Con ELENE ROSE, Vicente Garcia,...",
-    imagen: "./img_spotify/4/Con ELENE.png",
+    imagen: "./img_spotify/4/Con_ELENE.png",
     alt: "Con ELENE ROSE, Vicente Garcia",
   },
   {  //2    
     nombre: "Con Sin Bandera, Alex Ubago, Rayli Barba y...",
-    imagen: "./img_spotify/4/Con Sin Bandera.png",
+    imagen: "./img_spotify/4/Con_Sin_Bandera.png",
     alt: "Con Sin Bandera",
   },
   {  //3
     nombre: "Con Camilo, Piso 21, ELENA ROSE y más",
-    imagen: "./img_spotify/4/Con Camilo.png",
+    imagen: "./img_spotify/4/Con_Camilo.png",
     alt: "Con Camilo",
   },
   {  //4    
     nombre: "Con Piso 21, Beéle Alleh y más",
-    imagen: "./img_spotify/4/Con Piso 21.png",
+    imagen: "./img_spotify/4/Con_Piso_21.png",
     alt: "Con Piso 21",
   },
   {  //5    
     nombre: "Con Allen, Boza, Nicky Jam y más",
-    imagen: "./img_spotify/4/Con Allen.png",
+    imagen: "./img_spotify/4/Con_Allen.png",
     alt: "Con Allen, Boza, Nicky Jam y más",
   },
   {  //6   
     nombre: "Con Bad Bunny, Danny Ocean, Quevedo y...",
-    imagen: "./img_spotify/4/Con Bad Bunny.png",
+    imagen: "./img_spotify/4/Con_Bad_Bunny.png",
     alt: "Con Bad Bunny",
   },
   {  //7    
     nombre: "Con Tego Calderón, Angel y Khriz,...",
-    imagen: "./img_spotify/4/Con Tego Calderón.png",
+    imagen: "./img_spotify/4/Con_Tego_Calderon.png",
     alt: "Con Tego Calderón",
   },
   {  //8    
     nombre: "Con Feid, Manuel Turizo, Bas Bunny y...",
-    imagen: "./img_spotify/4/Con Feid Manuel.png",
+    imagen: "./img_spotify/4/Con_Feid_Manuel.png",
     alt: "Con Feid, Manuel",
   },
   {  //9    
     nombre: "Con Feid, Arcangel, KAROL G y más...",
-    imagen: "./img_spotify/4/Con Feid Arcangel.png",
+    imagen: "./img_spotify/4/Con_Feid_Arcangel.png",
     alt: "Con Feid, Arcangel",
   },
   {  //10
     nombre: "Con Bad Bunny, Quevedo, Karol G...",
-    imagen: "./img_spotify/4/Con Bad Bunny Quevedo.png",
+    imagen: "./img_spotify/4/Con_Bad_Bunny_Quevedo.png",
     alt: "Con Bad Bunny",
   }
 ];
